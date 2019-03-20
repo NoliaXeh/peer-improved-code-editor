172092ff7c324860b040b0f0b5bd81c672408aee
Thibault Marchal <thibault.marchal@epita.fr>
2019-03-20 19:00:16 +0100

    src/config/console : now uses curses

--- src/config/console.py
+++ src/config/console.py
@@ -1,4 +1,39 @@
 import os
+import sys
+import termios
+import tty
+
+import curses
+
+
+global screen
+screen = None
+
+def check_init(f, *args, **kwargs):
+    global screen
+    def ret(*args, **kwargs):
+        if screen == None:
+            raise Exception("console not initialized. Call console.init() beforhand")
+        return f(*args, **kwargs)
+    return ret
+
+def init():
+    global screen
+    screen = curses.initscr()
+    curses.def_prog_mode()
+    curses.noecho()
+    screen.keypad(True)
+
+
+@check_init
+def close():
+    global screen
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+
 
 def get_size():
     """
@@ -8,3 +43,18 @@ def get_size():
     """
     return os.popen('stty size', 'r').read().split()
 
+@check_init
+def set_cursor(line, column):
+    """
+        sets cursor on given position
+
+        Return: None
+    """
+    global screen
+    screen.move(line, column)
+
+@check_init
+def get_input():
+    global screen
+    screen.refresh()
+    return screen.getch()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,37 @@
 from config import console
+import time
+
+import curses
+
+screen = curses.initscr()
+console.init()
+
+print(curses.termname())
+
+left = curses.newwin(10, 10, 0, 0)
+right = curses.newwin(10, 10, 0, 10)
+left.border()
+right.box()
+
+#left.addstr(' '*99, curses.A_REVERSE)
+#right.addstr('.'*99, curses.COLOR_RED)
+
+left.refresh()
+right.refresh()
+
+console.screen.refresh()
+
+time.sleep(1)
+
+console.close()
+
+'''
+console.init()
 
 print(console.get_size())
+for i in range(5):
+    print(console.get_input())
 
 
+console.close()
+'''
